14a8fd4d0943f53d88f384a43e722579e37f4a59
cewilliams <cewilliams@csbsju.edu>
2019-04-17 21:04:15 -0500

    write to csv

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
+import csv
 from selenium import webdriver
 
-# open firefox browser
+# number of pages for results
+MAX_PAGE_NUM = 5
+# number of digits in url
+MAX_PAGE_DIG = 3
+
 driver = webdriver.Firefox()
 
-driver.get("http://econpy.pythonanywhere.com/ex/001.html")
+# create columns for buyers and sellers in csv
+with open('results.csv', 'w') as f:
+    f.write("Buyer, Price \n")
 
+# loop through page num to append to URL
+for i in range(1, MAX_PAGE_NUM + 1):
+    page_num = (MAX_PAGE_DIG - len(str(i))) * "0" + str(i)
+    url = "http://econpy.pythonanywhere.com/ex/" + page_num + ".html"
 
-# find elements by xpath
-buyers = driver.find_elements_by_xpath('//div[@title="buyer-name"]')
-prices = driver.find_elements_by_xpath('//span[@class="item-price"]')
+    driver.get(url)
 
-# print out all of the buyers and prices of the current page
+    buyers = driver.find_elements_by_xpath('//div[@title="buyer-name"]')
+    prices = driver.find_elements_by_xpath('//span[@class="item-price"]')
+    
+    # write to results to csv 
+    num_page_items = len(buyers)
+    with open('results.csv', 'a') as f:
+        for i in range(num_page_items):
+            f.write(buyers[i].text + "," + prices[i].text + "\n")
 
-num_page_items = len(buyers)
-for i in range(num_page_items):
-    print(buyers[i].text + " : " + prices[i].text)
-
-
+# end program
 driver.close()
-
-
-
